f8a3d1d4c5139b6b04ae4070285baea56b831868
Jukka Soikkeli <jes15@ic.ac.uk>
2017-10-13 11:15:21 +0100

    Updates to the microbit hub code

--- teacher_interface/mb_hub/src/microbit_chemistry_serial.js
+++ teacher_interface/mb_hub/src/microbit_chemistry_serial.js
@@ -1,26 +1,21 @@
-let curr_electron_bal = 0
 let send_str = ""
 let curr_symbol_int = 0
 let str_electron_bal = ""
 let curr_symbol = ""
-
 function get_chem_data()  {
     led_breathe()
     serial.writeValue("symbol_int", curr_symbol_int)
     curr_symbol = serial.readUntil(serial.delimiters(Delimiters.NewLine))
     str_electron_bal = serial.readUntil(serial.delimiters(Delimiters.NewLine))
-    curr_electron_bal = parseInt(str_electron_bal)
     send_str = "" + str_electron_bal + "_" + curr_symbol
     basic.showString("S:")
     basic.showString(send_str)
 }
-
 function chem_data_TEST()  {
     curr_symbol = "O"
     str_electron_bal = "-2"
     send_str = "" + str_electron_bal + "_" + curr_symbol
 }
-
 radio.onDataPacketReceived( ({ receivedString: name, receivedNumber: value }) =>  {
     if (name == "symbol_num") {
         curr_symbol_int = value
@@ -31,7 +26,6 @@ radio.onDataPacketReceived( ({ receivedString: name, receivedNumber: value }) =>
     }
     show_S()
 })
-
 function show_S()  {
     basic.showLeds(`
         . # # # .
@@ -41,16 +35,13 @@ function show_S()  {
         . # # # .
         `)
 }
-
 function send_chem_data()  {
     radio.sendValue(send_str, curr_symbol_int)
 }
-
 function get_compound_data()  {
     led_breathe()
     basic.showString("STUB")
 }
-
 function led_breathe()  {
     led.setBrightness(255)
     basic.pause(100)
@@ -66,10 +57,8 @@ function led_breathe()  {
     basic.pause(100)
     led.setBrightness(255)
 }
-
 radio.setGroup(83)
 radio.setTransmitPower(7)
 curr_symbol_int = 0
 curr_symbol = "H"
-curr_electron_bal = -1
 show_S()
