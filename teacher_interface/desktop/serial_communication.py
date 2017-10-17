e007c3c26449bff0dbb82e3a1eb4e013aa06f742
Jukka Soikkeli <jes15@ic.ac.uk>
2017-10-17 13:11:23 +0100

    Removing unnecessary clutter from serial_communication.py

--- teacher_interface/desktop/serial_communication.py
+++ teacher_interface/desktop/serial_communication.py
@@ -1,9 +1,7 @@
 import serial
 import numpy as np
 from pathlib import Path
-import time
 
-#import csv
 
 #the port will depend on your computer
 #for a raspberry pi it will probably be /dev/ttyACM0
@@ -36,13 +34,8 @@ else:
                              dtype=[('aliasID', 'S2'),('serialNo', 'i4'),\
                                     ('chem_symbol', 'S10'), ('valence', 'S2')])
 
-#print(master_table)
-#print("Valences are", master_table["valence"])
-
-
 ## Create the reaction table
 reaction_table = np.genfromtxt('elementsDB-v3.csv', delimiter=";", dtype=None)
-
 
 
 ## TESTING REACTIONS
@@ -98,13 +91,6 @@ print("Alias is", send_alias)
 #print(bytestr.decode())
 
 
-#with open('data.csv', newline='') as csvfile:
-#    symbols = csv.reader(csvfile, dialect='excel', delimiter=' ', quotechar='|')
-#    for row in spamreader:
-#        print(', '.join(row))
-
-
-
 # NEED:
 # 1. Send the ID numbers for the MBs
 #    -> need to have an if loop for this 
@@ -131,44 +117,36 @@ print("Alias is", send_alias)
 #   ac       1234567892  H2O           0
 
 
-# Make an event to update the GUI with this table every time this table gets updated
-#  - a check in the while loop that sends the info conditional on it being OK
-#   - the other code would be imported as a module for this
-
-
 # "MACROS"
 IDALIAS = 0
 ELEMENTREQ = 1
 REACTION_CHECK = 2
 
 next_ele = b'H' # TEMP DEF - make it relative to the master_table
-next_val = b'+1'
+next_val = b'+1' # TEMP
 
 try:
     while True:
         #read a line from the microbit, decode it and
         # strip the whitespace at the end
         data = s.readline().rstrip()
-
         print(data)
         
         #split the data
         data_s = data.split(b":")
-        #symbol_int = int(data_s[1])
-        symbol_int=2
-        
-        #print("Symbol int is:",symbol_int)
 
-        # Strip off the first elements to get the data type
+        # Split the data to comm_type, ID, and message
         # data_s[0] is comm_type, data_s[1] is ID, data_s[2] is the message
         comm_type = int((data_s[0]).decode())
         print("Comm type is:", comm_type)
         id_in = data_s[1]
         print("id_in", id_in)
         message = data_s[2] ##SPLIT this message to parts with message.split(b"#")
-        if comm_type == IDALIAS: ##NEED TO IMPLEMENT CHANGES TO mb and mb_hub codes
+
+        # Message handler
+        if comm_type == IDALIAS:
             SERIAL = id_in
-            # - CHECK THE TABLE FOR THE SERIAL, AND ALLOCATE ID NUMBER
+            # Check the table for the serial, and allocate an ID number
             if SERIAL in master_table["serialNo"]:
                 i, = np.where(master_table["serialNo"] == SERIAL)
                 send_alias = master_table["aliasID"][i][0]
@@ -176,7 +154,7 @@ try:
                 if -1 in master_table["serialNo"]:
                     i, = np.where(master_table["serialNo"] == -1)
                     send_alias = master_table["aliasID"][i][0]
-                else: # IF NO AVAILABLE ROW EXISTS
+                else: # if no available row exists
                     new_alias = bytes([master_table["aliasID"][-1][0], \
                                        master_table["aliasID"][-1][1]+1])
                     np.append(master_table, [new_alias, SERIAL, next_ele, next_val])
@@ -192,7 +170,6 @@ try:
             s.write(send_valence + send_symbol + b'\n')
         #  find "id_in" in the master table, and pick the element given to it, if any 
         elif comm_type == REACTION_CHECK:
-            #print("STUB -- check reaction table for reactions")
             message_s = message.split(b"#")
             id_a = message_s[0]
             id_b = message_s[1]
@@ -200,30 +177,35 @@ try:
             symb_a = master_table["chem_symbol"][x][0] + master_table["valence"][x][0]
             y, = np.where(master_table["aliasID"] == id_b)
             symb_b = master_table["chem_symbol"][y][0] + master_table["valence"][y][0]
-            #GET INDEX OF symb_a, then check if reaction_table[symb_b][symb_a] is empty or not
-            # -> NOT SURE how to find the index for id_a...
+            # Get the indices of the elements, then check if the location is empty
             i, = np.where(reaction_table[0,:] == symb_a)
             j, = np.where(reaction_table[:,0] == symb_b)
             if reaction_tablae[j,i] != b"":
                 print("Reaction accepted")
-                ##UPDATE THE MASTER TABLE, and send OK message
+                #Update the master table, and send OK message
                 master_table["chem_symbol"][x] = reaction_table[j,i]
                 master_table["chem_symbol"][y] = reaction_table[j,i]
                 master_table["valence"][x] = b'+0'
                 master_table["valence"][y] = b'+0'
-                s.write(b'1' + b'\n') ##OK message
-                ## THE HUB CAN ACCEPT THE OK, AND THEN REQUEST THE SYMBOLS AS BEFORE!
-                ## -> this means we have a check, and can recycle code
+                s.write(b'1' + b'\n') #OK message
+                # The hub can accept the OK, and simply request the symbol afterward
+                # -> this means we have a check, and can recycle code
             else:
                print("Reaction not allowed")
-               s.write(b'0'+b'\n') ## 'NOT OK' message
+               s.write(b'0'+b'\n') # 'NOT OK' message
         else: #other case or broken message, drop message and request resend?
             print("STUB - BROKEN MESSAGE?")
-            #  #DROP
+            #  DROP
 
             
         #IF THE MASTER_TABLE UPDATED:
         ##   UPDATE THE GUI WITH THE MASTER_TABLE DATA!
 
+
+# Make an event to update the GUI with this table every time this table gets updated
+#  - a check in the while loop that sends the info conditional on it being OK
+#   - the other code would be imported as a module for this
+
+        
 finally:
     s.close()
